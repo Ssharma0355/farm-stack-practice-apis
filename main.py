872409665da5d1d9681a4a4e254f9e0abf0e08ec
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-09-13 09:21:29 +0530

    Adding 2 apis with params and query

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return{"message":"Everything is ok with API till here"}
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int, q: Union[str,None] = None):
+    return {"item_id":item_id, "query":q}
+
+@app.get("/student/{student_id}")
+def get_student(student_id: int, q:Union[str, None] = None):
+    return {"Student Id is :":student_id, "Query is":q}
